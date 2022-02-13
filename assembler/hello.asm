939545a9bb515ca245e87d339647d238cdf3d13a
nabbbbi <nabbbbi@mail.ru>
2022-02-13 10:46:22 +0300

    added include commentary about include

--- assembler/hello.asm
+++ assembler/hello.asm
@@ -1,4 +1,4 @@
-	%include "stud_io.inc"
+	%include "stud_io.inc" {its a directive / file that told assembler implace it with all code that written within. This file written in assembler too. This pariticular file include macroses PRINT, PUTCHAR, FINISH. When assembler see this directive, he remember and became able use this macros until program's end}
 	global _start
 
 	section .text
